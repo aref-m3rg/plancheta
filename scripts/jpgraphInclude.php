f06330c50a2357a0d3ff6eb9621fd6b49c384b50
Martín Mirabete <UNPA.martinmirabete@gmail.com>
2026-08-05 11:44:40 -0300

    Se eliminaron los archivos de librerías y scripts de utilidades desuso (scripts/ y js/)  que no son utilizados ni referenciados en la aplicación.

--- scripts/jpgraphInclude.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include ("../../jpgraph/src/jpgraph.php");
-include ("../../jpgraph/src/jpgraph_bar.php");
-include ("../../jpgraph/src/jpgraph_pie.php");
-include ("../../jpgraph/src/jpgraph_pie3d.php");
-
-
-?>
